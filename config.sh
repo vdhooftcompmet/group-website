0e34db38de92c30ecbe5d7b916dc1d5b5b31432a
David Meijer <davidmeijer@Davids-MacBook-Pro-2.local>
2022-02-28 04:41:18 +0100

    add config.sh script

---
+++ config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+read -p "GitHub Pages url: " url
+
+regexp_url='^[A-Za-z_-]+\.github\.io$'
+if [[ "$url" =~ $regexp_url ]]; then 
+    IFS='.' read -r -a array <<< "$url"
+    username=${array[0]}
+    echo "Valid url '$url' for '$username'"
+
+    echo "configuring 'package.json'..."
+    filename="package.json"
+    full_url="https:\/\/"$url"\/"
+    regexp_json='("website"\: )"https\:\/\/[A-Za-z_-]+\.github\.io\/"'
+    sed -E -i "" "s/$regexp_json/\1\"$full_url\"/" package.json
+
+    echo "adding git remote 'website'..."
+    git remote add website "git@github.com:$username/$url.git"
+
+    echo "creating  'dev' branch..."
+    git checkout -b dev
+
+    echo "checkout 'dev' branch..."
+    git checkout dev
+
+else 
+    echo "Invalid url";
+    exit 1;
+fi
